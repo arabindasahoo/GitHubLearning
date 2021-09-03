a5340f3dc59c335589037c18de6c7a0001aebe04
arabindasahoo <arabindasahoo303@gmail.com>
2021-09-03 09:44:43 +0530

    user class added

---
+++ GitTestProject/src/main/java/com/git/testing/demo/entity/User.java
@@ -0,0 +1,67 @@
+package com.git.testing.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_details")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String phone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public User() {
+		super();
+	}
+
+	public User( String name, String email, String phone) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
+	
+	
+}
---
+++ GitTestProject/src/test/java/com/git/testing/demo/GitTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.testing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
